d832cef67c0765c984931eceb4bc8c4a8b172176
Stanislas Manscour <stanislas.manscour@medecindirect.fr>
2022-12-29 00:03:46 +0100

    use new port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-const port = 3000;
+const port = `0.0.0.0:$PORT`;
 
 io.on('connection', (socket) => {
   socket.on('message', msg => {
