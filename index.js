edacf3ef1d355c946921c511e49ee8a51ca96bce
Stanislas Manscour <stanislas.manscour@medecindirect.fr>
2022-12-28 23:52:30 +0100

    init backend

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = 3000;
+
+io.on('connection', (socket) => {
+  socket.on('message', msg => {
+    io.emit('message', msg)
+    console.log(msg);
+  })
+});
+
+
+
+server.listen(port, () => console.log(`server running on ${port}`));
