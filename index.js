608c1681122847ca6d057c58a9a057c4d38bda25
Stanislas Manscour <stanislas.manscour@medecindirect.fr>
2022-12-29 00:09:42 +0100

    fix PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-const port = `0.0.0.0:$PORT`;
+const port = process.env.PORT || 3000;
 
 io.on('connection', (socket) => {
   socket.on('message', msg => {
